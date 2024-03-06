7b8c2882c4487c45906d643982c65bdd5120e34e
Evelina <krugerevelina@gmail.com>
2024-03-06 19:02:12 +0100

    Account

---
+++ WebApp/Models/AccountAdressModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class AccountAdressModel
+    {
+        [DataType(DataType.Text)]
+        [Display(Name = "Adress", Prompt = "Enter your adress")]
+        [Required(ErrorMessage = "Required")]
+        public string AdressLine_1 { get; set; } = null!;
+
+        [DataType(DataType.Text)]
+        [Display(Name = "Second adress", Prompt = "Enter your second adress")]
+        public string? AdressLine_2{ get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "Postal code", Prompt = "Enter your postal code")]
+        [Required(ErrorMessage = "Required")]
+        public string PostalCode { get; set; } = null!;
+
+        [DataType(DataType.Text)]
+        [Display(Name = "City", Prompt = "Enter your city")]
+        [Required(ErrorMessage = "Required")]
+        public string City { get; set; } = null!;
+    }
+}
---
+++ WebApp/Models/AccountDetailsModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models;
+
+public class AccountDetailsModel
+{
+    [DataType(DataType.Text)]
+    [Display(Name = "First name", Prompt = "Enter your first name")]
+    [Required(ErrorMessage = "Required")]
+    public string FirstName { get; set; } = null!;
+
+    [DataType(DataType.Text)]
+    [Display(Name = "Last name", Prompt = "Enter your last name")]
+    [Required(ErrorMessage = "Required")]
+    public string LastName { get; set; } = null!;
+
+    [DataType(DataType.EmailAddress)]
+    [Display(Name = "Email", Prompt = "Enter your email")]
+    [Required(ErrorMessage = "Required")]
+    public string Email { get; set; } = null!;
+
+    [DataType(DataType.PhoneNumber)]
+    [Display(Name = "Phone number", Prompt = "Enter your phone number")]
+    [Required(ErrorMessage = "Required")]
+    public string Phone { get; set; } = null!;
+
+    [Display(Name = "Bio", Prompt = "Add a short bio...")]
+    [DataType(DataType.MultilineText)]
+    public string? Bio { get; set; }
+
+    [DataType(DataType.ImageUrl)]
+    public string? ProfileImage { get; set; }
+}
