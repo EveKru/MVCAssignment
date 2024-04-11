ae8060b0be21becc92f5835331bd910ab316bb72
Evelina <krugerevelina@gmail.com>
2024-04-11 17:42:01 +0200

    CookieConsent

---
+++ WebApp/Controllers/SiteSettingsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class SiteSettingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CookieConsent() 
+        {
+            var option = new CookieOptions
+            {
+                Expires = DateTime.Now.AddYears(1),
+                HttpOnly = true,
+                Secure = true
+            };
+            Response.Cookies.Append("CookieConsent", "true", option);
+            return Ok();
+        }
+    }
+}
